8ef15648c5b840ed744b98131f4cec534c604678
Aishnee katiyar <aishneekatiyar@gmail.com>
2026-07-06 22:11:36 +0530

    Completed All Doctors Page

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -12,8 +12,10 @@ const TopDoctors = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors</p>
         {/* <div className='w-full grid grid-cols-auto  gap-4 pt-5 gap-y-6 px-3 sm:px-0' > */}
         <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-5 px-3 sm:px-0'> 
-            {doctors.slice(0,10).map((item,index)=>(
-              <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 '  >
+            {doctors.slice(0,10).map((item)=>(
+              <div 
+              key={item._id}
+              onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 '  >
                 <img className='bg-blue-50' src={item.image} alt=""/>
                 <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
